c4b9148f831b33555a0661da9b90526a3e08b976
mogu5723 <hyun1sung2@naver.com>
2021-07-30 21:51:22 +0900

    add portal

--- Assets/Camera/CamearControl.cs
+++ Assets/Camera/CamearControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class MapArray{
@@ -9,13 +10,11 @@ public class MapArray{
 
 public class CamearControl : MonoBehaviour
 {
-    public Transform target;
-    float speed;
-    public MapArray[] chapter;
-    MapControl mCtrl;
+    public GameObject player; public Transform target;
+    public float speed;
+    public MapArray[] chapter; MapControl mCtrl;
     int[] mapCode = new int[2];
-    float height;
-    float width;
+    float height; float width;
    
     private void Awake() {
         speed = 3f;
@@ -26,8 +25,10 @@ public class CamearControl : MonoBehaviour
     }
     void Start()
     {
+        UIBlack.color = new Color(0, 0, 0, 1f);
         chapter[mapCode[0]].Map[mapCode[1]].SetActive(true);
         mCtrl = chapter[mapCode[0]].Map[mapCode[1]].GetComponent<MapControl>();
+        StartCoroutine(FadeIn());
     }
 
     float lx, ly;
@@ -42,5 +43,27 @@ public class CamearControl : MonoBehaviour
         clampY= Mathf.Clamp(transform.position.y, -ly + mCtrl.center.y, ly + mCtrl.center.y);
 
         transform.position = new Vector3(clampX, clampY, -10f);
+    }
+
+     public void PlayerSpon(int mapCode0, int mapCode1, float x, float y){
+         UIBlack.color = new Color(0, 0, 0, 1f);
+         chapter[mapCode[0]].Map[mapCode[1]].SetActive(false);
+         player.SetActive(false);
+
+         mapCode[0] = mapCode0; mapCode[1] = mapCode1;
+         chapter[mapCode0].Map[mapCode1].SetActive(true);
+         target.position = new Vector3(x, y, 0);
+         player.SetActive(true);
+         StartCoroutine(FadeIn());
+    }
+
+    public Image UIBlack;
+    IEnumerator FadeIn(){
+        float count = 1f;
+        while(count > 0){
+            yield return null;
+            count -= Time.deltaTime;
+            UIBlack.color = new Color(0, 0, 0, count);
+        }
     }
 }
